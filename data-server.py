2407772edb5b301e967046a00945472c6a577511
Pooya <pooya.darvehei@gmail.com>
2021-09-25 14:48:09 +0800

    made server cors compatible

--- data-server.py
+++ data-server.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 import json
 import csv
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 path = Path("./data")
 
 
